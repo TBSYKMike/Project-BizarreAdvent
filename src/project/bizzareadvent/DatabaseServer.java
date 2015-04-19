cee132dfbe5487dd46353b4b8ee1de4a4ff6c32d
TBSYKMike <Mike@Mike-Dator>
2015-04-19 16:50:12 +0200

    Added New Database Class, No codes yet This class should contain code for login and creating new account maybe also storing information

---
+++ src/project/bizzareadvent/DatabaseServer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.bizzareadvent;
+
+/**
+ *
+ * @author Mike
+ */
+public class DatabaseServer {
+    //kod för att inloggning och skapa ny konto och andra koder för att spara information
+}
