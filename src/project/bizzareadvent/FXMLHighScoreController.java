60ad261a4e29dbe44b8779bca2fdcd0e0e4bae56
TBSYKMike <Mike@Mike-Dator>
2015-04-24 01:46:12 +0200

    Added High Score scene. High Score scene loads values from DatabaseServer class.

---
+++ src/project/bizzareadvent/FXMLHighScoreController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.bizzareadvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import project.bizzareadvent.SaveLoad.DatabaseServer;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author Mike
+ */
+public class FXMLHighScoreController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+    @FXML
+    private TextArea textareaHighScore1, textareaHighScore2;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        String HighScore = DatabaseServer.getInstance().getHighScore();
+        String Username = DatabaseServer.getInstance().getUsername();
+        textareaHighScore1.setText(HighScore);
+        textareaHighScore2.setText(Username);
+    }    
+    
+}
--- src/project/bizzareadvent/SaveLoad/DatabaseServer.java
+++ src/project/bizzareadvent/SaveLoad/DatabaseServer.java
@@ -12,4 +12,54 @@ package project.bizzareadvent.SaveLoad;
 public class DatabaseServer {
     //kod för att inloggning och skapa ny konto och andra koder för att spara information
     
+    private static DatabaseServer instance;
+    
+    private DatabaseServer() {
+    }
+
+    public static DatabaseServer getInstance() {
+
+        if (instance == null) {
+            instance = new DatabaseServer();
+        }
+
+        return instance;
+
+    }
+    
+    private String HighScore = 
+                " 1\n"
+                + " 2\n"
+                + " 3\n"
+                + " 4\n"
+                + " 5\n"
+                + " 6\n"
+                + " 7\n"
+                + " 8\n"
+                + " 9\n"
+                + "10";
+    
+    private String Username = 
+                " Username  1\n"
+                + " Username  2\n"
+                + " Username  3\n"
+                + " Username  4\n"
+                + " Username  5\n"
+                + " Username  6\n"
+                + " Username  7\n"
+                + " Username  8\n"
+                + " Username  9\n"
+                + " Username 10";
+
+    public String getHighScore() {
+        return HighScore;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+    
+    
+    
+    
 }
