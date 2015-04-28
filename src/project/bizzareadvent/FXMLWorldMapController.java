b7bd149566441c4a6090a4a1204e3d744f7cec0d
Erik <Deciver_89@hotmail.com>
2015-04-29 00:17:28 +0200

    Added back and city button in world map, cleaned up the scene

--- src/project/bizzareadvent/FXMLWorldMapController.java
+++ src/project/bizzareadvent/FXMLWorldMapController.java
@@ -5,15 +5,81 @@
  */
 package project.bizzareadvent;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class FXMLWorldMapController implements Initializable {
+    @FXML
+     private Button buttonBack;
+    @FXML
+     private Button buttonCity;
+    @FXML
+     private Button buttonAdventure;
+    @FXML
+    private Label currentHp;
+    @FXML
+    private Label attack;
+    @FXML
+    private Label defens;
+    @FXML
+    private Label currentGold;
+    @FXML
+    private Label currentScore;
+    @FXML
+    private ImageView background;
+    @FXML
+    private ImageView worldMap;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
+    }
+    
+    @FXML
+    public void handleButtonActionBack(ActionEvent event) {
+        try {
+            Node node = (Node) event.getSource();
+            Stage stageLogin = (Stage) node.getScene().getWindow();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLScene2.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            stageLogin.setScene(scene);
+            stageLogin.show();
+
+        } catch (IOException ex) {
+            System.out.println("Scene change error1");
+        }
+    }
+    @FXML
+    public void handleButtonActionCity(ActionEvent event) {
+        try {
+            Node node = (Node) event.getSource();
+            Stage stageLogin = (Stage) node.getScene().getWindow();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLCity.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            stageLogin.setScene(scene);
+            stageLogin.show();
+
+        } catch (IOException ex) {
+            System.out.println("Scene change error1");
+        }
     }
 
 }
