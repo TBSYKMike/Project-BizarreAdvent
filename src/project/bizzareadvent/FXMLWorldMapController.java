aa82b0b1cfc13c2b89e85d1b9ff6773e5e89c381
Erik <Deciver_89@hotmail.com>
2015-05-06 10:33:15 +0200

    HATE GIT!!!!!!!

--- src/project/bizzareadvent/FXMLWorldMapController.java
+++ src/project/bizzareadvent/FXMLWorldMapController.java
@@ -5,6 +5,7 @@
  */
 package project.bizzareadvent;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,22 +18,34 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class FXMLWorldMapController implements Initializable {
+
     @FXML
-     private Button buttonBack;
+    private Button buttonBack;
     @FXML
-     private Button buttonCity;
+    private Button buttonCity;
     @FXML
-     private Button buttonAdventure;
+    private Button buttonAdventure;
+    @FXML
+    private Label hpLabel;
+    @FXML
+    private Label attackLabel;
+    @FXML
+    private Label defensLabel;
+    @FXML
+    private Label goldLabel;
+    @FXML
+    private Label scoreLabel;
     @FXML
     private Label currentHp;
     @FXML
-    private Label attack;
+    private Label currentAttack;
     @FXML
-    private Label defens;
+    private Label currentDefens;
     @FXML
     private Label currentGold;
     @FXML
@@ -41,12 +54,41 @@ public class FXMLWorldMapController implements Initializable {
     private ImageView background;
     @FXML
     private ImageView worldMap;
+    @FXML
+    private ImageView mapPosition1;
+    @FXML
+    private ImageView mapPosition2;
+    @FXML
+    private ImageView mapPosition3;
+    @FXML
+    private ImageView mapPosition4;
+    @FXML
+    private ImageView mapPosition5;
+
+    boolean position1 = false;
+    boolean position2 = false;
+    boolean position3 = false;
+    boolean position4 = false;
+    boolean position5 = false;
+
+    File file = new File("src/warrior.jpg");
+    Image image = new Image(file.toURI().toString());
+
+    File file2 = new File("src/possiblePosition.jpg");
+    Image image2 = new Image(file2.toURI().toString());
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        mapPosition1.setImage(image);
+        mapPosition2.setImage(image2);
+        mapPosition3.setImage(image2);
+        mapPosition4.setImage(image2);
+        mapPosition5.setImage(image2);
+        
+        
 
     }
-    
+
     @FXML
     public void handleButtonActionBack(ActionEvent event) {
         try {
@@ -64,6 +106,7 @@ public class FXMLWorldMapController implements Initializable {
             System.out.println("Scene change error1");
         }
     }
+
     @FXML
     public void handleButtonActionCity(ActionEvent event) {
         try {
@@ -81,7 +124,7 @@ public class FXMLWorldMapController implements Initializable {
             System.out.println("Scene change error1");
         }
     }
-    
+
     @FXML
     public void handleButtonActionAdventure(ActionEvent event) {
         try {
@@ -97,6 +140,104 @@ public class FXMLWorldMapController implements Initializable {
 
         } catch (IOException ex) {
             System.out.println("Scene change error1");
+        }
+    }
+
+    public void movementAction1(ActionEvent event) {
+
+        position1 = true;
+        position2 = false;
+        position3 = false;
+        position4 = false;
+        position5 = false;
+
+        updatePosition();
+    }
+
+    public void movementAction2(ActionEvent event) {
+
+        position1 = false;
+        position2 = true;
+        position3 = false;
+        position4 = false;
+        position5 = false;
+
+        updatePosition();
+    }
+
+    public void movementAction3(ActionEvent event) {
+
+        position1 = false;
+        position2 = false;
+        position3 = true;
+        position4 = false;
+        position5 = false;
+
+        updatePosition();
+    }
+
+    public void movementAction4(ActionEvent event) {
+
+        position1 = false;
+        position2 = false;
+        position3 = false;
+        position4 = true;
+        position5 = false;
+
+        updatePosition();
+    }
+
+    public void movementAction5(ActionEvent event) {
+
+        position1 = false;
+        position2 = false;
+        position3 = false;
+        position4 = false;
+        position5 = true;
+
+        updatePosition();
+    }
+
+    public void updatePosition() {
+        if (position1 = true) {
+            mapPosition1.setImage(image);
+            mapPosition2.setImage(image2);
+            mapPosition3.setImage(image2);
+            mapPosition4.setImage(image2);
+            mapPosition5.setImage(image2);
+
+        }
+        if (position2 = true) {
+            mapPosition1.setImage(image2);
+            mapPosition2.setImage(image);
+            mapPosition3.setImage(image2);
+            mapPosition4.setImage(image2);
+            mapPosition5.setImage(image2);
+
+        }
+        if (position3 = true) {
+            mapPosition1.setImage(image2);
+            mapPosition2.setImage(image2);
+            mapPosition3.setImage(image);
+            mapPosition4.setImage(image2);
+            mapPosition5.setImage(image2);
+
+        }
+        if (position4 = true) {
+            mapPosition1.setImage(image2);
+            mapPosition2.setImage(image2);
+            mapPosition3.setImage(image2);
+            mapPosition4.setImage(image);
+            mapPosition5.setImage(image2);
+
+        }
+        if (position5 = true) {
+            mapPosition1.setImage(image2);
+            mapPosition2.setImage(image2);
+            mapPosition3.setImage(image2);
+            mapPosition4.setImage(image2);
+            mapPosition5.setImage(image);
+
         }
     }
 }
