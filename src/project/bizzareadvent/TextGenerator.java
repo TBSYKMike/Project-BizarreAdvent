9fdfe14bd8014e519e7235e5bd4cc6d52c015210
Jocke90 <roslund1990@gmail.com>
2015-05-21 13:28:38 +0200

    Added some story in the TextGenerator class.

--- src/project/bizzareadvent/TextGenerator.java
+++ src/project/bizzareadvent/TextGenerator.java
@@ -30,13 +30,35 @@ public class TextGenerator {
     }
     
     private String plainsText(){ // NEED TO ADD STORY
-        String plainsText = "lol";
-        
+        String plainsText;
+            if(randomInt < 50){
+                plainsText = ("\n\nYou keep following the path you once started on"
+                            + "\nNothing more interesting to be told here, press that continue button.");
+            }
+            else{
+                plainsText = ("\n\nYou pass more purple rocks that has strange formations."
+                            + "\nYou giggle as you pass the rocks as the formations amuse you.");
+            }
         return plainsText;
     }
     
     private String forestText(){ // NEED TO ADD STORY
-        String forestText = "lol";
+        String forestText;
+        
+        if(randomInt <= 33){
+            forestText = ("\n\nYou keep following a path through the forest."
+                        + "\nIt is abit quiet, almost to quiet.");
+        }
+        else if(randomInt > 33 && randomInt <= 66){
+            forestText = ("\n\nYou stop a moment, trying to figure out which direction you should take."
+                        + "\nIt all looks the same, trees there, trees over there and trees over here."
+                        + "\nAfter minutes thinking you just take a direction and hope for the best.");
+        }
+        else{
+            forestText = ("\n\nYou feel lost and scared, but not alone."
+                        + "\nYou can spot eyes glowing here and there, lurking at the trees."
+                        + "\nDo wonder what kind of creatures that live here in the forest of Adabûn.");
+        }
         
         return forestText;
     }
@@ -46,13 +68,21 @@ public class TextGenerator {
         
         if(randomInt <= 50){
             swampText = ("\n\nYou keep walking through swamp."
-                    + "'Ugh this is so disgusting!'"
-                    + "You are wet and cold but you need to keep moving.");
-        }else{
+                        + "\n'Ugh this is so disgusting!'"
+                        + "\nYou are wet and cold but you need to keep moving.");
+        }
+        else if(randomInt > 45 && randomInt <= 90){
             swampText = ("\n\nYou struggle to make your way through the swamp,"
-                        + "stumble over what seems to be roots or fallen trees."
-                        + "Hard to tell as your vision is restricted by the fog");
+                        + "\nstumble over what seems to be roots or fallen trees."
+                        + "\nHard to tell as your vision is restricted by the fog");
             
+        }
+        else{
+            swampText = ("\n\nAs you keep making your way through the swamp,"
+                        + "\nyou hear this creepy voice over and over again."
+                        + "\n'Carry on you must little noob!'"
+                        + "\nWho or what might that be?"
+                        + "\nYou just ignore it and keep on going.");
         }
         return swampText;
     }
@@ -105,8 +135,27 @@ public class TextGenerator {
     }
     
     private String castleText(){ // NEED TO ADD STORY
-        String castleText = "lol";
+        String castleText;
         
+        if(randomInt <= 33){
+            castleText = ("\n\nYou keep moving through a long hallway,"
+                        + "\nit's lit up with torches and has a funky smell to it."
+                        + "\nYou have reached the end of the hallway and proceed through a door.");
+        }
+        else if(randomInt > 33 && randomInt <= 66){
+            castleText = ("\n\nYou have entered what seems to be a torturechamber."
+                        + "\nWeird machines, devices and tools that makes your stomach turn."
+                        + "\n'This is fucked up!'"
+                        + "\nYou have seen enough of this room and you decide to go back through the door you came.");
+        }
+        else{
+            castleText = ("\n\nYou enter a large room, with long tables, alot of chairs, paintings on the walls"
+                        + "\nand an open fireplace."
+                        + "\nIt is a dining hall!"
+                        + "\nYou look around and find some food laying on the tables."
+                        + "\nYou take some food and sit down next to the fireplace for abit,"
+                        + "\nbefore you continue to explore more of the castle.");
+        }
         return castleText;
     }
 }
